d52b4adfc223874249971b0e243802d88cb82557
Kruck <reka_931@mail.ru>
2023-06-04 21:35:18 +0300

    Добавление описания кода и функций.

--- src/vacancy.py
+++ src/vacancy.py
@@ -2,15 +2,19 @@ import re
 
 
 class Vacancy:
-    def __init__(self, title, url, salary, work, salary_currency):
+    """Класс вакансии"""
+    def __init__(self, title, url, salary, description, salary_currency):
+        """Инициализация экземпляра"""
         self.title = self.validate_title(title)
         self.url = self.validate_url(url)
         self.salary = salary
-        self.requirements = self.validate_work(work)
+        self.description = self.validate_description(description)
         self.salary_currency = self.validate_salary_currency(salary_currency)
 
     @classmethod
     def from_dict(cls, data, source):
+        """Формирует экземпляры вакансий из словарей с сайтов hh.ru и superjob.ru"""
+# Получаем информацию с hh.ru
         if source == 'hh':
             title = data.get('name')
             url = data.get('alternate_url')
@@ -27,6 +31,7 @@ class Vacancy:
                 salary = "Отсутствует"
                 salary_currency = "Не указано"
             work = data['snippet'].get('requirement')
+# Получаем информацию с superjob.ru
         elif source == 'sj':
             title = data.get('profession')
             url = data.get('link')
@@ -39,18 +44,21 @@ class Vacancy:
 
     @staticmethod
     def validate_title(title):
+        """Проверяет названия вакансии"""
         if not title:
             raise ValueError("Title cannot be empty.")
         return title
 
     @staticmethod
     def validate_url(url):
+        """Проверяет ссылку вакансии"""
         if not url.startswith("http"):
             raise ValueError("URL is not valid.")
         return url
 
     @staticmethod
     def validate_salary(salary):
+        """Проверяет правильность зарплаты"""
         if not salary:
             return None
         pattern = r"(\d+\D*(\d+)?"
@@ -66,14 +74,16 @@ class Vacancy:
             raise ValueError(f"Некорректный формат зарплаты: {salary}")
 
     @staticmethod
-    def validate_work(work):
-        if not work:
+    def validate_description(description):
+        """Проверяет описание вакансии"""
+        if not description:
             return None
         else:
-            return work
+            return description
 
     @staticmethod
     def validate_salary_currency(salary_currency):
+        """Проверяет валюту зарплаты"""
         if not salary_currency:
             return None
         elif len(salary_currency) > 3:
@@ -91,5 +101,5 @@ class Vacancy:
 Ссылка: {self.url}, 
 Зарплата от: {self.salary['min']}  Зарплата до: {self.salary['max']}
 Валюта: {self.salary_currency}, 
-Описание: {self.requirements}
+Описание: {self.description}
 """
